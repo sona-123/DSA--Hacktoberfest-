428b739d353d124790e381cfb9f65709ac71c43a
Sonali <sonalibyahut0125@gmail.com>
2022-10-13 22:00:22 +0530

    solved an issue

---
+++ Python/min_max_element.py
@@ -0,0 +1,9 @@
+
+import numpy
+arr = numpy.array([10, 5, 14, 8, 23, 7])
+max_element = numpy.max(arr)
+min_element = numpy.min(arr)
+print('maxi element in the array is: ',
+	max_element)
+print('mini element in the array is: ',
+	min_element)
